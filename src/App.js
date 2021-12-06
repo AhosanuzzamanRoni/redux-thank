e91c852b5734f9009150f79d2c8fd1b685c6647f
AhosanuzzamanRoni <86812185+AhosanuzzamanRoni@users.noreply.github.com>
2021-12-06 21:37:58 +0600

    redux-shop using redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import Header from './Components/Header';
+import ProductListing from './Components/ProductListing';
+import './App.css'
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import ProductDetails from './Components/ProductDetails';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       
+      <BrowserRouter>
+      <Header/>
+      
+   
+      <Routes>
+      <Route path="/" element={<ProductListing />}/>
+      <Route path="/product/:productId" element={<ProductDetails/>}/>
+      <Route>  404 Not Found!...</Route>
+
+      </Routes>
+     
+      </BrowserRouter>
+   
+     
+      
     </div>
   );
-}
+};
 
 export default App;
